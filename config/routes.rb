9724f39d2ae80ef9f1642e64f772eff34e5d2299
Denis Naughton <mjolinor75@hotmail.com>
2020-10-18 17:28:09 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/knippenblogger'
+  get 'static_pages/events'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
