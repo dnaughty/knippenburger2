aadc3d32cb1eb76591494c006de55a8737372aff
Denis Naughton <mjolinor75@hotmail.com>
2020-10-19 02:01:40 -0400

    Added title helper, pruned navbar

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Knippenburger"
+    assert_select "title", "Knippenburger"
   end
 
   test "should get knippenblogger" do
